211617c8b8ba2ebbdc1ba60302cb811e9df0bb12
Peacemaker228 <alanchik99@gmail.com>
2024-11-03 01:01:11 +0300

    layout: added TestsPage and constants

--- src/app/tests/page.tsx
+++ src/app/tests/page.tsx
@@ -4,127 +4,77 @@ import React, { useState } from 'react'
 import { Calendar } from '@/app/calendar/Calendar'
 import { Heading } from '@/components/ui/Heading'
 import { Wrapper } from '@/components/wrapper'
-import Select, { SingleValue } from 'react-select'
 import { ITestCardProps, TestCard } from '@/components/test-card'
-import { DropdownIndicator } from '@/components/drop-down-indicator'
+import { cardData, initialSortValue } from '@/app/tests/constants'
+import { FormProvider, useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
+import { Filters } from '@/components/layout/tests-page/Filters'
+import { Panel } from '@/components/layout/tests-page/Panel'
+import { CreateMaterialModal } from '@/components/layout/tests-page/CreateMaterialModal'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ITestsFilter } from '@/models/testsFilterSchema'
+import { createMaterialSchema, ICreateMaterial } from '@/models/createMaterialSchema'
 
-interface ISort {
-  type: IValueLabelModel
-  date: IValueLabelModel
-}
-
-interface IValueLabelModel {
+export interface IValueLabelModel {
   value: string
   label: string
 }
 
-const initialSortValue: ISort = {
-  type: { value: 'Тест', label: 'Тест' },
-  date: { value: 'Раньше', label: 'Раньше' },
+export interface ISort {
+  type: IValueLabelModel
+  date: IValueLabelModel
 }
 
-const typeOption = [
-  {
-    value: 'Тест',
-    label: 'Тест',
-  },
-  {
-    value: 'Опрос',
-    label: 'Опрос',
-  },
-]
-
-const dateOption = [
-  {
-    value: 'Раньше',
-    label: 'Раньше',
-  },
-  {
-    value: 'Позже',
-    label: 'Позже',
-  },
-]
-
-const cardData = [
-  {
-    id: 1,
-    title: 'Тест по завершению терапии',
-    type: 'test',
-    date: '20.12.2024',
-  },
-  {
-    id: 2,
-    title: 'Опрос по завершению терапии',
-    type: 'survey',
-    date: '20.12.2024',
-  },
-  {
-    id: 3,
-    title: 'Тест промежуточный результат',
-    type: 'test',
-    date: '20.12.2024',
-  },
-  {
-    id: 4,
-    title: 'Опрос промежуточный результат',
-    type: 'survey',
-    date: '20.12.2024',
-  },
-  {
-    id: 5,
-    title: 'Анкета определения мотивации',
-    type: 'form',
-    date: '20.12.2024',
-  },
-]
 const TestsPage = () => {
   const [sort, setSort] = useState<ISort>(initialSortValue)
+  const [modalOpen, setModalOpen] = useState(false)
 
-  const handleSortChange = (key: keyof ISort, value: SingleValue<IValueLabelModel>) => {
-    setSort((prev) => ({
-      ...prev,
-      [key]: value,
-    }))
+  const handlePanelSubmit = (values: ITestsFilter) => {
+    console.log(values)
   }
+
+  const handleCreateSubmit = (values: ICreateMaterial) => {
+    console.log(values)
+    toast.success('Тест успешно создан!')
+    setTimeout(() => {
+      setModalOpen(false)
+    }, 1000)
+  }
+
+  const form = useForm<ITestsFilter>({
+    defaultValues: {
+      search: '',
+      type: 'Опросник',
+    },
+  })
+
+  const createMaterialForm = useForm<ICreateMaterial>({
+    resolver: zodResolver(createMaterialSchema),
+    defaultValues: {
+      name: '',
+      type: '',
+    },
+  })
 
   return (
     <Calendar>
       <Wrapper>
         <Heading title="Опросники и тесты" />
-        <div className="flex gap-[28px] mt-[31px] mb-[8px]">
-          <div className="flex gap-[8px] items-center">
-            <p>Сортировать по типу</p>{' '}
-            <Select
-              value={sort.type}
-              onChange={(value) => {
-                handleSortChange('type', value)
-              }}
-              options={typeOption}
-              styles={{
-                indicatorSeparator: () => ({ display: 'none' }),
-              }}
-              components={{ DropdownIndicator }}
-            />
+        <Filters sort={sort} setSort={setSort} />
+        <div className="flex gap-x-[20px] items-start">
+          <div className="main_content bg-[#F1F1F1] rounded-[5px] py-[12px] px-[16px] flex flex-col gap-y-[12px]">
+            {cardData.map((el) => (
+              <TestCard key={el.id} type={el.type as ITestCardProps['type']} title={el.title} date={el.date} />
+            ))}
           </div>
-          <div className="flex gap-[8px] items-center">
-            <p>По дате создания</p>{' '}
-            <Select
-              value={sort.date}
-              onChange={(value) => {
-                handleSortChange('date', value)
-              }}
-              options={dateOption}
-              styles={{
-                indicatorSeparator: () => ({ display: 'none' }),
-              }}
-              components={{ DropdownIndicator }}
-            />
+          <div className="flex flex-col items-start gap-y-[20px] w-full max-w-[364px]">
+            <FormProvider {...form}>
+              <Panel onSubmit={handlePanelSubmit} />
+            </FormProvider>
+            <FormProvider {...createMaterialForm}>
+              <CreateMaterialModal onSubmit={handleCreateSubmit} modalOpen={modalOpen} setModalOpen={setModalOpen} />
+            </FormProvider>
           </div>
-        </div>
-        <div className="main_content bg-[#F1F1F1] rounded-[5px] py-[12px] px-[16px] flex flex-col gap-y-[12px]">
-          {cardData.map((el) => (
-            <TestCard key={el.id} type={el.type as ITestCardProps['type']} title={el.title} date={el.date} />
-          ))}
         </div>
       </Wrapper>
     </Calendar>
