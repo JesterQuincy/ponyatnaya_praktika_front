85db923741a07372c9236c31240810aa3258709e
ArtycoBW <artyco.w.w@gmail.com>
2024-11-03 13:15:47 +0300

    adding the "анкета клиента" page

--- src/components/QuestionnaireCard/index.tsx
+++ src/components/QuestionnaireCard/index.tsx
@@ -7,9 +7,9 @@ interface QuestionnaireCardProps {
 
 const QuestionnaireCard: React.FC<QuestionnaireCardProps> = ({ title, children }) => {
   return (
-    <div className="bg-white py-6 px-10 rounded-xl shadow-md w-full max-w-screen-2xl">
+    <div className="bg-white py-4 px-10 rounded-xl shadow-md w-full max-w-screen-2xl max-h-[96vh] overflow-auto">
       <h1 className="text-[35px] font-bold mb-3 font-ebgaramond">{title}</h1>
-      <div className="bg-grey px-8 py-5 rounded-[4px]">{children}</div>
+      <div className="bg-grey px-8 py-3 rounded-[4px]">{children}</div>
     </div>
   )
 }
