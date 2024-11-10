5b6f17364b1f73a3e59fad7405ee4054eea23d50
Peacemaker228 <alanchik99@gmail.com>
2024-11-10 22:28:32 +0300

    layout: child card segment

---
+++ src/api/hooks/child/useUpdateChild.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'react-toastify'
+import { clientService } from '@/services/clients.service'
+import { IClient } from '@/types/clients'
+import { EInvalidationTags } from '@/api/hooks/constants'
+import { IChild } from '@/types/child'
+import { childService } from '@/services/child.service'
+import { DeepPartial } from '@/types/common'
+
+export function useUpdateChild() {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationKey: [EInvalidationTags.CHILD],
+    mutationFn: (data: DeepPartial<IChild>) => childService.updateChild(data),
+    onSuccess: () => {
+      toast.success('Вы успешно обновили клиента')
+      queryClient.invalidateQueries({ queryKey: [EInvalidationTags.CHILD] })
+    },
+    onError: () => {
+      toast.error('Ошибка при обновлении клиента')
+    },
+  })
+}
