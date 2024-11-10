bb993a6f744badde45f3e94fb2b838c85c2a9bdd
Peacemaker228 <alanchik99@gmail.com>
2024-11-10 22:31:15 +0300

    layout: couple card segments

---
+++ src/components/layout/card-page/Couple/SelectCouple.tsx
@@ -0,0 +1,58 @@
+import Select from 'react-select'
+import { IValueLabelModel } from '@/models/ILabelValueModel'
+import { FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
+import { UseFormReturn } from 'react-hook-form'
+import { NestedKeys } from '@/types/common'
+import { ICoupleSchema } from '@/models/coupleSchema'
+
+interface ISelectHandlerProps {
+  options?: IValueLabelModel[]
+  form: UseFormReturn<ICoupleSchema>
+  name: NestedKeys<ICoupleSchema> // Используем тип NestedKeys для вложенных ключей
+  label: string
+}
+
+export function SelectCouple({ options, form, name, label }: ISelectHandlerProps) {
+  return (
+    <>
+      <span>{label}</span>
+      <FormField
+        name={name}
+        control={form.control}
+        render={({ field }) => {
+          const selectedOption = options?.find((option) => option.value === field.value) || null
+
+          return (
+            <FormItem>
+              <FormControl>
+                <Select
+                  {...field}
+                  onChange={(option) => {
+                    field.onChange((option as IValueLabelModel)?.value)
+                    form.trigger(name)
+                  }}
+                  options={options}
+                  placeholder="Выберите"
+                  value={selectedOption}
+                  styles={{
+                    control: (base) => ({
+                      ...base,
+                      height: '36px',
+                      minHeight: '36px',
+                      padding: '0 0 5px 0',
+                      borderRadius: '12px',
+                      maxWidth: '129px',
+                      fontSize: '15px',
+                    }),
+                    indicatorSeparator: () => ({ display: 'none' }),
+                  }}
+                />
+              </FormControl>
+              <FormMessage className="text-orange" />
+            </FormItem>
+          )
+        }}
+      />
+    </>
+  )
+}
