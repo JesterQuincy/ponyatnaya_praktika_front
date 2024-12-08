06f87b19aae41b3e574cc264d1b7e10751e94b1f
Peacemaker228 <alanchik99@gmail.com>
2024-12-09 00:31:33 +0300

    feat: methodsService update

--- src/services/methodsService.ts
+++ src/services/methodsService.ts
@@ -1,13 +1,34 @@
 import { axiosWithAuth } from '@/api/interceptors'
-import { TMethodsArray } from '@/types/methods'
-import { IMeetProjMethod } from '@/types/methods/meetMethods'
+import { IMethodic, TMethodsArray } from '@/types/methods'
+import { IMeetProjMethod, IProjectiveMethod } from '@/types/methods/meetMethods'
+import { DeepPartial, TPromiseNumber } from '@/types/common'
 
 export const methodsService = {
   async getAllTypes(): Promise<{ data: TMethodsArray }> {
     return await axiosWithAuth.get('/api/innerOptions/typeMethod/getAllTypes')
   },
 
+  async getMethodById(id: number): Promise<{ data: IProjectiveMethod }> {
+    return await axiosWithAuth.get(`/api/innerOptions/projectiveMethods/get/${id}`)
+  },
+
+  async createType(data: DeepPartial<IMethodic>): TPromiseNumber {
+    return await axiosWithAuth.post('/api/innerOptions/typeMethod', data)
+  },
+
+  async createProjMethod(data: DeepPartial<IMeetProjMethod>): TPromiseNumber {
+    return await axiosWithAuth.post('/api/innerOptions/projectiveMethods', data)
+  },
+
+  async updateProjMethod(data: DeepPartial<IMeetProjMethod>): TPromiseNumber {
+    return await axiosWithAuth.put('/api/innerOptions/projectiveMethods', data)
+  },
+
+  async deleteProjMethod(id: number) {
+    return await axiosWithAuth.delete(`/api/innerOptions/projectiveMethods/delete/${id}`)
+  },
+
   async getMeetProjMethods(meetId: number): Promise<{ data: IMeetProjMethod[] }> {
-    return await axiosWithAuth.get(`/api/v1/General/projMethods/${meetId}`)
+    return await axiosWithAuth.get(`/api/innerOptions/projectiveMethods/byMeet/${meetId}`)
   },
 }
