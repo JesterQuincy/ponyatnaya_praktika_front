2cbaba31fedf5693b05dbe6ed982735e3bfb4883
Peacemaker228 <alanchik99@gmail.com>
2024-12-11 03:20:39 +0300

    fix: added MeetWithSuspense

--- src/app/meet/page.tsx
+++ src/app/meet/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { Calendar } from '@/app/calendar/Calendar'
-import { Meet } from '@/app/meet/Meet'
+import { MeetWithSuspense } from '@/app/meet/Meet'
 import { Wrapper } from '@/components/wrapper'
 
 export default function MeetPage() {
   return (
     <Calendar>
       <Wrapper>
-        <Meet />
+        <MeetWithSuspense />
       </Wrapper>
     </Calendar>
   )
