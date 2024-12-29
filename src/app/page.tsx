15276e4f60b974806772b61b062e815a7efccc6d
ArtycoBW <artyco.w.w@gmail.com>
2024-12-30 01:44:30 +0300

    refactor: set login page as homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
 import './page.css'
-import '@radix-ui/themes/styles.css'
+export { default } from './(auth)/login/page';
 
-export default function Home() {
-  return <main className="main"></main>
-}
