616e503cbe045423c027b46e40acd26fb157fd24
alan.batchaev <alanchik99@gmail.com>
2025-02-24 14:54:22 +0300

    fix: date picker

--- src/components/ui/datePicker.tsx
+++ src/components/ui/datePicker.tsx
@@ -16,6 +16,8 @@ interface DatePickerProps {
   placeholder?: string
   className?: string
   formatDate?: 'dd.MM.yyyy' | 'yyyy-MM-dd'
+  fromYear?: number
+  toYear?: number
 }
 
 function DatePicker({
@@ -24,6 +26,8 @@ function DatePicker({
   placeholder = 'Выберите дату',
   className,
   formatDate = 'dd.MM.yyyy',
+  fromYear = 1900,
+  toYear = new Date().getFullYear(),
 }: DatePickerProps) {
   const selectedDate = value ? parse(value, formatDate, new Date()) : undefined
 
@@ -47,8 +51,18 @@ function DatePicker({
           <Image src={CalendarIcon} alt="Calendar icon" width={15} height={16} />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0 bg-white border-[#b2b2b2] rounded-[6px]" align="start">
-        <Calendar mode="single" selected={selectedDate} onSelect={handleDateChange} initialFocus />
+      <PopoverContent id={'date-picker'} className="w-auto p-0 bg-white border-[#b2b2b2] rounded-[6px]" align="start">
+        <Calendar
+          id={'date-picker'}
+          mode="single"
+          selected={selectedDate}
+          onSelect={handleDateChange}
+          initialFocus
+          captionLayout="dropdown-buttons"
+          fromYear={fromYear}
+          toYear={toYear}
+          locale={ru}
+        />
       </PopoverContent>
     </Popover>
   )
