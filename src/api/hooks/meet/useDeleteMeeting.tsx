b400674c2147dd34511f119bff13545dd6550867
alan.batchaev <alanchik99@gmail.com>
2025-07-12 13:43:24 +0300

    feat: deleting meeting & meeting list card fixes

---
+++ src/api/hooks/meet/useDeleteMeeting.tsx
@@ -0,0 +1,15 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { EInvalidationTags } from '@/api/hooks/constants'
+import { meetingService } from '@/services/meet.sevice'
+
+export const useDeleteMeeting = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationKey: [EInvalidationTags.MEET_ID],
+    mutationFn: (id: number) => meetingService.deleteMeeting(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [EInvalidationTags.MEET_ID] })
+    },
+  })
+}
--- src/components/ui/forms/DropDownMenu.tsx
+++ src/components/ui/forms/DropDownMenu.tsx
@@ -1,11 +1,42 @@
-import { useEffect, useRef } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
+import { useDeleteMeeting } from '@/api/hooks/meet/useDeleteMeeting'
+import { toast } from 'react-toastify'
 
 interface DropdownMenuProps {
   onClose: () => void
+  id: number
 }
 
-export function DropdownMenu({ onClose }: DropdownMenuProps) {
+export function DropdownMenu({ onClose, id }: DropdownMenuProps) {
+  const { mutateAsync: deleteMeeting } = useDeleteMeeting()
+
   const menuRef = useRef<HTMLDivElement>(null)
+
+  const handleDelete = useCallback(async () => {
+    if (!id) return
+
+    const toastId = toast.loading('Удаление встречи...')
+
+    try {
+      await deleteMeeting(id)
+
+      toast.update(toastId, {
+        render: 'Вы успешно удалили встречу',
+        type: 'success',
+        isLoading: false,
+        autoClose: 3000,
+      })
+    } catch {
+      toast.update(toastId, {
+        render: 'Ошибка при удалении встречи',
+        type: 'error',
+        isLoading: false,
+        autoClose: 5000,
+      })
+    } finally {
+      onClose()
+    }
+  }, [deleteMeeting, id, onClose])
 
   useEffect(() => {
     // Функция для закрытия при клике вне меню
@@ -26,19 +57,7 @@ export function DropdownMenu({ onClose }: DropdownMenuProps) {
   return (
     <div ref={menuRef} className="absolute right-[20px] top-[40px] bg-white shadow-lg rounded-lg z-10 w-[150px] py-2">
       <ul className="flex flex-col gap-1">
-        <li className="px-4 py-2 hover:bg-[#D9D9D9] cursor-pointer text-sm" onClick={onClose}>
-          Просмотреть
-        </li>
-        <li className="px-4 py-2 hover:bg-[#D9D9D9] cursor-pointer text-sm" onClick={onClose}>
-          Редактировать
-        </li>
-        <li className="px-4 py-2 hover:bg-[#D9D9D9] cursor-pointer text-sm" onClick={onClose}>
-          Создать копию
-        </li>
-        <li className="px-4 py-2 hover:bg-[#D9D9D9] cursor-pointer text-sm" onClick={onClose}>
-          Архивировать
-        </li>
-        <li className="px-4 py-2 hover:bg-[#D9D9D9] cursor-pointer text-sm" onClick={onClose}>
+        <li className="px-4 py-2 hover:bg-[#D9D9D9] cursor-pointer text-sm" onClick={handleDelete}>
           Удалить
         </li>
       </ul>
