604593f255160285f73b880169fe72fb01e76056
Арсен Тангатаров <arsen01998@yandex.ru>
2025-09-15 17:38:22 +0300

    fix

--- src/components/poll/PollActionsPanel.tsx
+++ src/components/poll/PollActionsPanel.tsx
@@ -9,12 +9,7 @@ const PollActionsPanel = () => {
 
       <div className="mt-7">
         <h1 className="text-xl font-semibold">Заметка к материалу</h1>
-        <Textarea
-          className="border-gray rounded-[6px] max-h-[25vh] h-[15vh]"
-          placeholder={`Этот опрос создан специально для клиента: 
-${'\n'}Продолжить разбираться в истории Анны и выявить возможные триггеры ее тревоги.
-${'\n'}Использовать методы релаксации (например, `}
-        />
+        <Textarea className="border-gray rounded-[6px] max-h-[25vh] h-[15vh]" />
       </div>
     </div>
   )
